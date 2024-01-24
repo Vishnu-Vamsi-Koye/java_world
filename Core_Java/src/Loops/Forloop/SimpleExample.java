a556a757d41a62272e753af75a58bdae882a5538
Vishnu-Vamsi-Koye <vishnuvamsi.0912@gmail.com>
2024-01-24 18:56:11 -0500

    New Program added

---
+++ Core_Java/src/Loops/Forloop/SimpleExample.java
@@ -0,0 +1,11 @@
+package Loops.Forloop;
+
+public class SimpleExample {
+    public static void main(String[] args) {
+
+        for (int i=1; i<=100;i++)
+        {
+            System.out.println(i);
+        }
+    }
+}

ed0703eed6ae9c063d3398fe8157e528f8b64272
Vishnu-Vamsi-Koye <vishnuvamsi.0912@gmail.com>
2024-01-25 18:34:50 -0500

    Code Changed

--- Core_Java/src/Loops/Forloop/SimpleExample.java
+++ Core_Java/src/Loops/Forloop/SimpleExample.java
@@ -2,10 +2,11 @@ package Loops.Forloop;
 
 public class SimpleExample {
     public static void main(String[] args) {
+        System.out.println("Printing Numbers from 1 to 100");
 
         for (int i=1; i<=100;i++)
         {
-        	System.out.println("Printing Numbers from 1 to 100");
+
             System.out.println(i);
         }
     }
